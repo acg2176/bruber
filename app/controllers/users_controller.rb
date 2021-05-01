0fbea7640e822afb7b4d0c1f9425050d01b3d3b0
Anna Guerrero <acg2176@columbia.edu>
2021-05-01 16:46:36 -0400

    add user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+    #    binding.pry
+       #insert into the db
+       @user = User.new(user_params)
+       if @user.save
+        #log them in
+        session[:user_id] = @user.id
+        #redirect to show page
+        redirect_to @user, :alert => "You Just signed Up"
+       else
+        render :new
+       end
+       #user save
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    private #some are permitted and some are not
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
 end
