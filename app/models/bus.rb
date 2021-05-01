875886fc992c7ff038274e132d774b3eb975e21e
Anna Guerrero <acg2176@columbia.edu>
2021-05-01 16:28:55 -0400

    edit bus model

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,3 +1,6 @@
 class Bus < ApplicationRecord
-  belongs_to :driver
+  belongs_to :driver #current_driver
+  has_many :shifts
+  has_many :drivers, through: :shifts
+  has_many :bus_trips, through: :shifts
 end
