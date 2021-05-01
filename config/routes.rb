a109ae0f3a56e8e65624d03cd0f30060cf0c3d7e
Anna Guerrero <acg2176@columbia.edu>
2021-05-01 17:02:25 -0400

    add login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
 
+  #login
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+  #logout
+  delete '/logout' => 'sessions#destroy'
 
 
 
