81529394522171031999ec8ba9017f7c69c98f65
Anna Guerrero <acg2176@columbia.edu>
2021-05-02 00:41:29 -0400

    add sessions controller login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,12 @@ class SessionsController < ApplicationController
     def home
     end
 
+    def new
+    end
+
+    def create
+    end
+
     def destroy
         # binding.pry
         session.delete(:user_id)
