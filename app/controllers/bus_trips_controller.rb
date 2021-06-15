64767840e42a06cf62b21a3bbdc8c091d493979d
Anna Guerrero <acg2176@columbia.edu>
2021-06-15 00:11:52 -0400

    fix bus trips

--- app/controllers/bus_trips_controller.rb
+++ app/controllers/bus_trips_controller.rb
@@ -6,12 +6,27 @@ class BusTripsController < ApplicationController
     end
 
     def create
+        # binding.pry
+        
+
         @bus_trip = current_user.bus_trips.build(bus_trip_params)
+        
+        #add the price here instead?
+        # ((params[:trip_end_time] - @bus_trip[:trip_start_time]) /60).to_i 
+        if ((@bus_trip[:trip_end_time]-@bus_trip[:trip_start_time])/60).to_i <= 60
+            @bus_trip[:price] = 10
+        else
+            @bus_trip[:price] = 50
+        end
+        start_time = @bus_trip[:trip_start_time]
+        end_time = @bus_trip[:trip_end_time]
+        
+        @bus_trip[:trip_start_time] = start_time.to_datetime
+        @bus_trip[:trip_end_time] = end_time.to_datetime
         binding.pry
        if @bus_trip.save
         #redirect to show page
-        binding.pry
-        redirect_to bus_trip_path
+        redirect_to bus_trips_path
        else
         render :new
        end
@@ -19,9 +34,16 @@ class BusTripsController < ApplicationController
     end
 
     private
+    def add_price
+        if ((bus_trip_params[:trip_end_time]-bus_trip_params[:trip_start_time])/60).to_i <= 60
+            bus_trip_params[:price] = 10
+        else
+            bus_trip_params[:price] = 50
+        end
+    end
 
     def bus_trip_params
-        params.require(:bus_trip).permit(:trip_start_time, :trip_end_time, :start_location, :end_location)
+        params.require(:bus_trip).permit(:trip_start_time, :trip_end_time, :start_location, :end_location, :price)
         # "trip_start_time(1i)"=>"2021", "trip_start_time(2i)"=>"6", "trip_start_time(3i)"=>"15", 
         # "trip_start_time(4i)"=>"06", "trip_start_time(5i)"=>"00", "trip_end_time(1i)"=>"2021", "trip_end_time(2i)"=>"6", "trip_end_time(3i)"=>"15", "trip_end_time(4i)"=>"06", 
         # "trip_end_time(5i)"=>"30", "start_location"=>"New York", "end_location"=>"Queens"
